cd4c1935b1dcd3ad7cf6dbf79daf209dda138c78
aniketKankekar7285 <aniket92000@gmail.com>
2023-11-26 23:38:31 +0530

    used InjectMocks,mock,extendwith annotation

--- src/test/java/business/BusinessimplTestUsingMock.java
+++ src/test/java/business/BusinessimplTestUsingMock.java
@@ -3,20 +3,22 @@ package business;
 import data.dataService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class BusinessimplTestUsingMock {
+    @InjectMocks
     Businessimpl business =new Businessimpl();
-    dataService someDataServiceMock=mock(dataService.class);
-    @BeforeEach
-    public void before()
-    {
-        business.setSomedataservice(someDataServiceMock);
-    }
+    @Mock
+    dataService someDataServiceMock;
+
 
 
     @Test
